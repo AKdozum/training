ba63ea01ab22f2864501c1c75bcc7f76f228cea8
shin-en <van.s.ver@gmail.com>
2013-09-11 22:48:46 +0900

    first commit

---
+++ app.psgi
@@ -0,0 +1,20 @@
+use FindBin;
+use lib "$FindBin::Bin/extlib/lib/perl5";
+use lib "$FindBin::Bin/lib";
+use File::Basename;
+use Plack::Builder;
+use DenaTraining::Web;
+
+my $root_dir = File::Basename::dirname(__FILE__);
+
+my $app = DenaTraining::Web->psgi($root_dir);
+builder {
+    enable 'ReverseProxy';
+    enable 'Static',
+        path => qr!^/(?:(?:css|js|img)/|favicon\.ico$)!,
+        root => $root_dir . '/public';
+    $app;
+};
+
+
+
---
+++ lib/DenaTraining/Web.pm
@@ -0,0 +1,69 @@
+package DenaTraining::Web;
+
+use strict;
+use warnings;
+use utf8;
+use Kossy;
+use Teng;
+use Teng::Schema::Loader;
+
+
+filter 'set_title' => sub {
+  my $app = shift;
+  sub {
+    my ( $self, $c )  = @_;
+    $c->stash->{site_name} = __PACKAGE__;
+    $app->($self,$c);
+  }
+};
+
+get '/' => [qw/set_title/] => sub {
+  my ( $self, $c )  = @_;
+
+  $c->render('index.tx', { greeting => "Hello" });
+};
+
+post '/save' => sub {
+  my ($self, $c) = @_;
+  my $result = $c->req->validator([
+      'tbox' => {
+        rule => [
+          ['NOT_NULL','Please enter something!'],
+        ],
+      }
+    ]);
+  if ( $result->has_error ) {
+    return $c->render_json({ 
+        error => 1, 
+        messages => $result->errors });
+  }
+  my $teng = &init;
+  $teng->insert('comments' => {
+      'comment' => $result->valid->get('tbox')
+    });
+  $c->render('save.tx', { 
+      text => $result->valid->get('tbox') 
+    });
+};
+
+
+sub init {
+  my $self = shift;
+  my $dsn    = 'dbi:mysql:dena_training';
+  my $user   = 'root';
+  my $passwd = '';
+  my $dbh = DBI->connect($dsn, $user, $passwd, {
+      'mysql_enable_utf8' => 1,
+    });
+  my $teng = Teng::Schema::Loader->load(
+    'dbh'       => $dbh,
+    'namespace' => 'MyApp::DB',
+  );
+
+  return $teng;
+}
+
+
+
+1;
+
